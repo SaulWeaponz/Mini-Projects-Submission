ab3b599e496d0d9a2f671aa0d37a2306151f14f6
SaulWeaponz <saulweaponz@gmail.com>
2025-06-05 13:43:00 +0300

    Adding of Random Quote Generator Files

---
+++ Random_Quote_Generator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import RandomQuote from "./components/RandomQuote/RandomQuote"
+
+
+function App() {
+
+
+  return (
+    <div>
+      <RandomQuote/>
+    </div>
+  )
+}
+
+export default App
